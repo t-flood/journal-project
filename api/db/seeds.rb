5d26bab19afd6c77e6249ec656c443d3fa202592
Tim Flood <tflood9@gmail.com>
2021-04-08 14:18:20 -0400

    create rails api and react app

---
+++ api/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Entry.create(title: "Monday", content: "Some content 1")
+Entry.create(title: "Tuesday", content: "Some content 2")
+Entry.create(title: "Wednesday", content: "Some content 3")
+Entry.create(title: "Thursday", content: "Some content 4")
+Entry.create(title: "Friday", content: "Some content 5")
