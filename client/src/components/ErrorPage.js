d81b349277b11fa081dba0c791c76d233ce96938
Tim Flood <tflood9@gmail.com>
2021-04-12 13:30:49 -0400

    added styling for error page

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class Error extends Component {
   render() {
-    return <div>Error</div>;
+    return (
+      <div class="col-md-12 mb-4">
+        <div class="alert alert-danger" role="alert">
+          There was an error!
+        </div>
+      </div>
+    );
   }
 }
 
